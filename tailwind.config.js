ba2db9f5772755173066254f54f644bea2fa90c7
Hari Kishore <97150417+hari-kishore8@users.noreply.github.com>
2024-07-25 19:58:19 +0530

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Fredoka-One": ['"Fredoka One"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
